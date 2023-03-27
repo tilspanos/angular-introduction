db9e4c70563b8b66b6679ec18eea9b10b26c803f
Tilemachos Vlasios Spanos <t.v.spanos2@gmail.com>
2023-03-27 21:01:58 +0300

    Update on Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-introduction';
 }
+
+@Component({
+  selector: 'app-hello',
+  template: `<div class="border border-2 border-success"><p>Hello how are you?</p></div>`,
+})
+export class HelloComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent, HelloComponent} from './app.component';
+import { MyHelloComponentComponent } from './my-hello-component/my-hello-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        HelloComponent,
+        MyHelloComponentComponent
+    ],
   imports: [
     BrowserModule
   ],
